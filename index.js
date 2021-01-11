563e7f5ed632aeed4d603348ca489618454d849f
Jhn <jhonata.tenorio2526@gmail.com>
2021-01-11 03:47:34 -0300

    all information

--- index.js
+++ index.js
@@ -3,9 +3,64 @@ const puppeteer = require('puppeteer');
 const start = async () => {
     const browser = await puppeteer.launch( { headless: false } );
     const page = await browser.newPage();
-    await page.goto('https://news.google.com/covid19/map?hl=pt-BR&gl=BR&ceid=BR%3Apt-419&mid=%2Fm%2F09c7w0');
+    await page.goto('https://covid.saude.gov.br/');
    
-    // await browser.close();
+    const container = await page.evaluate(() => {
+        const updateDate = document.querySelectorAll(".display-flex.justify-between.item-line.width-full.pd-left")[1].querySelectorAll(".lb-nome.lb-value")[4].innerText.split("\n")[1];    
+        const recoveredCases = document.querySelectorAll(".lb-total.tp-geral")[0].innerText;
+        const accompaniment = document.querySelectorAll(".lb-total.tp-geral")[1].innerText;
+        const accumulated = document.querySelectorAll(".lb-total.tp-geral")[2].innerText;
+        const newCases = document.querySelectorAll(".lb-total.tp-geral")[3].innerText;
+        const accidentalDeaths = document.querySelectorAll(".lb-total.tp-geral")[5].innerText;
+        const newDeaths = document.querySelectorAll(".lb-total.tp-geral")[6].innerText;
+
+        const regs = {
+                sul: {
+                    cases: document.querySelectorAll(".display-flex.justify-between.item-line.width-full.pd-left")[1].querySelectorAll(".lb-nome.lb-value")[0].innerText.split("\n"),
+                    deaths: document.querySelectorAll(".display-flex.justify-between.item-line.width-full.pd-left")[1].querySelectorAll(".lb-nome.lb-value")[1].innerText.split("\n"),    
+                    incidence: document.querySelectorAll(".display-flex.justify-between.item-line.width-full.pd-left")[1].querySelectorAll(".lb-nome.lb-value")[2].innerText.split("\n"), 
+                    mortality: document.querySelectorAll(".display-flex.justify-between.item-line.width-full.pd-left")[1].querySelectorAll(".lb-nome.lb-value")[3].innerText.split("\n"), 
+                },
+                centro_oeste: {
+                    cases: document.querySelectorAll(".display-flex.justify-between.item-line.width-full.pd-left")[3].querySelectorAll(".lb-nome.lb-value")[0].innerText.split("\n"),
+                    deaths: document.querySelectorAll(".display-flex.justify-between.item-line.width-full.pd-left")[3].querySelectorAll(".lb-nome.lb-value")[1].innerText.split("\n"),    
+                    incidence: document.querySelectorAll(".display-flex.justify-between.item-line.width-full.pd-left")[3].querySelectorAll(".lb-nome.lb-value")[2].innerText.split("\n"), 
+                    mortality: document.querySelectorAll(".display-flex.justify-between.item-line.width-full.pd-left")[3].querySelectorAll(".lb-nome.lb-value")[3].innerText.split("\n"), 
+                },
+                nordeste: {
+                    cases: document.querySelectorAll(".display-flex.justify-between.item-line.width-full.pd-left")[2].querySelectorAll(".lb-nome.lb-value")[0].innerText.split("\n"),
+                    deaths: document.querySelectorAll(".display-flex.justify-between.item-line.width-full.pd-left")[2].querySelectorAll(".lb-nome.lb-value")[1].innerText.split("\n"),    
+                    incidence: document.querySelectorAll(".display-flex.justify-between.item-line.width-full.pd-left")[2].querySelectorAll(".lb-nome.lb-value")[2].innerText.split("\n"), 
+                    mortality: document.querySelectorAll(".display-flex.justify-between.item-line.width-full.pd-left")[2].querySelectorAll(".lb-nome.lb-value")[3].innerText.split("\n"), 
+                },
+                sudeste: {
+                    cases: document.querySelectorAll(".display-flex.justify-between.item-line.width-full.pd-left")[0].querySelectorAll(".lb-nome.lb-value")[0].innerText.split("\n"),
+                    deaths: document.querySelectorAll(".display-flex.justify-between.item-line.width-full.pd-left")[0].querySelectorAll(".lb-nome.lb-value")[1].innerText.split("\n"),    
+                    incidence: document.querySelectorAll(".display-flex.justify-between.item-line.width-full.pd-left")[0].querySelectorAll(".lb-nome.lb-value")[2].innerText.split("\n"), 
+                    mortality: document.querySelectorAll(".display-flex.justify-between.item-line.width-full.pd-left")[0].querySelectorAll(".lb-nome.lb-value")[3].innerText.split("\n"), 
+                },
+                norte: {
+                    cases: document.querySelectorAll(".display-flex.justify-between.item-line.width-full.pd-left")[4].querySelectorAll(".lb-nome.lb-value")[0].innerText.split("\n"),
+                    deaths: document.querySelectorAll(".display-flex.justify-between.item-line.width-full.pd-left")[4].querySelectorAll(".lb-nome.lb-value")[1].innerText.split("\n"),    
+                    incidence: document.querySelectorAll(".display-flex.justify-between.item-line.width-full.pd-left")[4].querySelectorAll(".lb-nome.lb-value")[2].innerText.split("\n"), 
+                    mortality: document.querySelectorAll(".display-flex.justify-between.item-line.width-full.pd-left")[4].querySelectorAll(".lb-nome.lb-value")[3].innerText.split("\n"), 
+                },
+        }
+        
+        return {
+            updateDate: updateDate,
+            recoveredCases: recoveredCases.split("\n")[1],
+            accompaniment: accompaniment.split("\n")[1],
+            accumulated: accumulated.split("\n")[0],
+            newCases: newCases.split("\n")[0],
+            accidentalDeaths: accidentalDeaths.split("\n")[0],
+            newDeaths: newDeaths.split("\n")[0],
+            regs: regs
+        }
+    });
+    
+    await browser.close();
+    console.log(container);
 };
 
 start();
