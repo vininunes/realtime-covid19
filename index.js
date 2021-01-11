1ba3e8732900dd2197c8f2fe2167383de34c5bf8
vini <viniciusnunes1709@gmail.com>
2021-01-11 02:46:45 -0300

    get information

--- index.js
+++ index.js
@@ -9,11 +9,26 @@ const start = async () => {
         const recoveredCases = document.querySelectorAll(".lb-total.tp-geral")[0].innerText;
         const accompaniment = document.querySelectorAll(".lb-total.tp-geral")[1].innerText;
         const accumulated = document.querySelectorAll(".lb-total.tp-geral")[2].innerText;
-        return [recoveredCases, accompaniment]
+        const newCases = document.querySelectorAll(".lb-total.tp-geral")[3].innerText;
+        const accidentalDeaths = document.querySelectorAll(".lb-total.tp-geral")[5].innerText;
+        const newDeaths = document.querySelectorAll(".lb-total.tp-geral")[6].innerText;
+        return {
+            recoveredCases: recoveredCases.split("\n")[1],
+            accompaniment: accompaniment.split("\n")[1],
+            accumulated: accumulated.split("\n")[0],
+            newCases: newCases.split("\n")[0],
+            accidentalDeaths: accidentalDeaths.split("\n")[0],
+            newDeaths: newDeaths.split("\n")[0]
+        }
     });
     
-    console.log(container[0]);
-    console.log(container[1]);
+    // console.log(container.recoveredCases);
+    // console.log(container.accompaniment);
+    // console.log(container.accumulated);
+    // console.log(container.newCases);
+    // console.log(container.accidentalDeaths);
+    // console.log(container.newDeaths);
+    console.table(container)
     await browser.close();
 };
 
